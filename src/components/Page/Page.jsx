cf8567f832d6adb3b9d07eeb9dd8d827fd996792
Paulo Vitor Cruz <paulocsvitor@gmail.com>
2022-01-03 09:44:59 -0300

    add pageTitle

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -5,14 +5,30 @@ import { Container, SlideFade } from '@chakra-ui/react';
 import { useScroll } from '../../hooks/useScroll';
 
 const Page = props => {
-  const { children, title, description, ...containerProps } = props;
+  const { children, title, ...containerProps } = props;
   const { hasScroll } = useScroll();
+
+  const pageDescription = process.env.APP_DESCRIPTION;
+  const pageTitle = title
+    ? `${process.env.APP_TITLE} - ${title}`
+    : process.env.APP_TITLE;
 
   return (
     <>
-      <Head title={title} subtitle={description} />
+      <Head>
+        <title>{pageTitle}</title>
+        <meta property="og:title" content={pageTitle} key="title" />
+        <meta name="description" content={pageDescription} />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <SlideFade in={true}>
-        <Container {...containerProps} pt={hasScroll ? "100px" : "50px"} minH="74vh">{children}</Container>
+        <Container
+          {...containerProps}
+          pt={hasScroll ? '100px' : '50px'}
+          minH="74vh"
+        >
+          {children}
+        </Container>
       </SlideFade>
     </>
   );
